56dc95d12ddecf588b5238c87108d02e3f7567b7
Olivia Tran <olivia.tran@walmart.com>
2022-12-29 22:45:57 -0800

    add styling to home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import styled from '@emotion/styled'
 
+const Button = styled.button`
+background: #AA96DA;
+color: white;
+border-radius: 1rem;
+display: inline-block;
+font-size: 2rem;
+margin: 1rem;
+padding: 0.25rem 1rem;
+
+`
+
+const Container = styled.div`
+background: #A8D8EA;
+color: white;
+text-align: center;
+font-weight: bold;
+&:hover {
+  color: #AA96DA;
+}
+
+
+
+`
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <div>
+    <Container>
+     <h4>React Project OT</h4>
+  <Button> Click me!</Button>
+  </Container>
+  </div>
 }
 
 export default App
